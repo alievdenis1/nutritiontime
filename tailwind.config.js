f40d3a181230e065c7ac1c8d52b33f8ccf42bfb4
Vyacheslav <vgaysin@abyka.com>
2023-06-05 08:38:48 +0500

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
 	],
 	theme: {
 		extend: {},
+		colors: {
+			black: '#000000',
+			white: '#FFFFFF',
+			brown: '#968D7D',
+			beige: '#FBF3E6',
+			green: '#056760',
+			red: '#E75854',
+			yellow: '#FFD274'
+		},
+		fontSize: {
+			default: '17px',
+			small: '15px',
+			big: '18px',
+			title: '28px'
+		}
 	},
 	plugins: [],
 }
