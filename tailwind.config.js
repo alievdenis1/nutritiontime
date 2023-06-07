de9d8670013d009f593adec1f1af55cb7e48afed
Vyacheslav <vgaysin@abyka.com>
2023-06-07 20:53:05 +0500

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,11 @@ export default {
 			yellow: '#FFD274'
 		},
 		fontSize: {
-			default: '17px',
-			small: '15px',
-			big: '18px',
-			title: '28px'
+			base: ['17px', '20px'],
+			sm: ['15px', '18px'],
+			xs: ['12px', '14px'],
+			lg: ['18px', '21px'],
+			title: ['28px', '32px']
 		}
 	},
 	plugins: [],
