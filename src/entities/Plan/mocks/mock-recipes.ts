432dfcede11a283c78911c9e89ec32f8bc38004d
Samvel <samveld396@gmail.com>
2024-06-27 18:20:47 +0300

    fix_styling correction

--- src/entities/Plan/mocks/mock-recipes.ts
+++ src/entities/Plan/mocks/mock-recipes.ts
@@ -3,59 +3,59 @@ import { RecipeList } from '../ui/type'
 export const mockRecipes: RecipeList = [
     {
         'id': 1,
-        'image': '../../../../public/image/recipes1.png',
+        'image': '/image/recipes1.png',
         'title': 'Вкуснейший домашний бургер',
         'rating': '5.0',
         'time': '35 мин',
         'calories': '300 ккал',
         'author': {
             'name': 'Денис Алиев',
-            'profile_image': '../../../../public/image/denis.svg'
+            'profile_image': '/image/denis.svg'
         },
         'comments': 0,
         'bookmarked': false,
         'liked': false
     },
     {
-        'id': 1,
-        'image': '../../../../public/image/recipes1.png',
+        'id': 2,
+        'image': '/image/recipes1.png',
         'title': 'Вкуснейший домашний бургер',
         'rating': '5.0',
         'time': '35 мин',
         'calories': '300 ккал',
         'author': {
             'name': 'Денис Алиев',
-            'profile_image': '../../../../public/image/denis.svg'
+            'profile_image': '/image/denis.svg'
         },
         'comments': 0,
         'bookmarked': false,
         'liked': false
     },
     {
-        'id': 1,
-        'image': '../../../../public/image/recipes1.png',
+        'id': 3,
+        'image': '/image/recipes1.png',
         'title': 'Вкуснейший домашний бургер',
         'rating': '5.0',
         'time': '35 мин',
         'calories': '300 ккал',
         'author': {
             'name': 'Денис Алиев',
-            'profile_image': '../../../../public/image/denis.svg'
+            'profile_image': '/image/denis.svg'
         },
         'comments': 0,
         'bookmarked': false,
         'liked': false
     },
     {
-        'id': 1,
-        'image': '../../../../public/image/recipes1.png',
+        'id': 4,
+        'image': '/image/recipes1.png',
         'title': 'Вкуснейший домашний бургер',
         'rating': '5.0',
         'time': '35 мин',
         'calories': '300 ккал',
         'author': {
             'name': 'Денис Алиев',
-            'profile_image': '../../../../public/image/denis.svg'
+            'profile_image': '/image/denis.svg'
         },
         'comments': 0,
         'bookmarked': false,
