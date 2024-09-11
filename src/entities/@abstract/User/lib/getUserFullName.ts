20161411cc913c8ab6c406c9becefcd2ad4c511c
denisaliev <alievdenis1@ya.ru>
2024-09-11 18:07:41 +0700

    удалил ненужное

--- src/entities/@abstract/User/lib/getUserFullName.ts
+++
@@ -1,7 +0,0 @@
-export const getUserFullName = (firstname: string, lastname: string, surname: string | null) => {
-    let fullName = lastname ? `${lastname} ` : ''
-    fullName += firstname ? `${firstname} ` : ''
-    fullName += surname || ''
-
-    return fullName.trim()
-}
