f2f1e6afb4c923ef210ddedb89c3d60d0086b6d1
denisaliev <alievdenis1@ya.ru>
2025-04-14 13:15:32 +0800

    фикс авторизации

--- src/features/Auth/log-in/api/index.ts
+++ src/features/Auth/log-in/api/index.ts
@@ -1,12 +1,15 @@
 // src/api/auth.ts
 import useApi from '@/shared/lib/api/use-api'
 import type { User } from '@/entities/Session/types'
+import WebApp from '@twa-dev/sdk'
+type TWAInitData = typeof WebApp.initData
 
 export interface LoginResponse {
 	user: User;
 	token: string;
 }
 interface LoginData {
+	initData: TWAInitData
 	telegram_id: string
 	first_name: string
 	last_name: string
@@ -14,6 +17,8 @@ interface LoginData {
 }
 
 export function login(data: LoginData) {
+	console.log('LoginData')
+	console.log(data)
 	return useApi<LoginResponse>('post', '/login', data)
 }
 
